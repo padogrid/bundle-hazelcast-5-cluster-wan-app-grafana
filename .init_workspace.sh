67b38bd16310176b398a9a9f488c06377fa7c1b9
javapark1 <javapark7@gmail.com>
2023-10-28 08:47:32 -0400

    Upadated bundle initialization logic

--- .init_workspace.sh
+++ .init_workspace.sh
@@ -1,13 +1,46 @@
 #!/usr/bin/env bash
 
-#
-# Add 3 members to each cluster
-#
-clusters="myhz1 myhz2 wan1 wan2"
-for i in $clusters; do
-   cluster_count=$(show_cluster -no-color -cluster $i | grep "Members Running" | awk '{print $3}' | sed 's/^.*\///')
-   count=$((3-$cluster_count))
-   if [ $count -gt 0 ]; then
-      add_member -cluster $i -count $count
+
+# Workspace name passed in by 'install_bundle'
+WORKSPACE="$1"
+
+ERROR_OCCURED="false"
+if [ "$WORKSPACE" == "" ]; then
+   echo >&2 "ERROR: Workspace not specified. The first argument must be the target workspace name. Command aborted."
+   ERROR_OCCURED="true"
+fi
+
+if [ "$ERROR_OCCURED" == "false" ]; then
+   FOUND="false"
+   WORKSPACES=$(list_workspaces)
+   for i in $WORKSPACES; do
+      if [ "$i" == "$WORKSPACE" ]; then
+         FOUND="true"
+      fi
+   done
+
+   if [ "$FOUND" == "false" ]; then
+      echo >&2 "ERROR: Specified workspace not found in the current RWE [$WORKSPACE]. Command aborted."
+      ERROR_OCCURED="true"
    fi
-done
+fi
+
+if [ "$ERROR_OCCURED" == "false" ]; then
+   switch_workspace $WORKSPACE
+
+   #
+   # Add 3 members to each cluster
+   #
+   clusters="myhz1 myhz2 wan1 wan2"
+   for i in $clusters; do
+      cluster_count=$(show_cluster -no-color -cluster $i | grep "Members Running" | awk '{print $3}' | sed 's/^.*\///')
+      count=$((3-$cluster_count))
+      if [ $count -gt 0 ]; then
+         add_member -cluster $i -count $count
+      fi
+   done
+
+   echo ""
+   echo "Bundle workspace initialization complete [$WORKSPACE]."
+   echo ""
+fi
